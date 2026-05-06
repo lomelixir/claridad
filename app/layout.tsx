92b20f83a9ad0494023ac44e177a24ea67f0673c
LOMELINUX <kevin.lome@hotmail.com>
2026-05-06 17:52:27 -0600

    fix background on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -39,27 +38,7 @@ export default function RootLayout({
       lang="es"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        <div className="fixed inset-0 -z-10">
-          <Image
-            src="/bgMobile.png"
-            alt=""
-            fill
-            sizes="(max-width: 767px) 100vw, 0px"
-            priority
-            className="object-cover md:hidden"
-          />
-          <Image
-            src="/bgDesktop.png"
-            alt=""
-            fill
-            sizes="(min-width: 768px) 100vw, 0px"
-            priority
-            className="object-cover hidden md:block"
-          />
-        </div>
-        {children}
-      </body>
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
